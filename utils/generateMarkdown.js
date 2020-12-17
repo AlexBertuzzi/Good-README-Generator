ee6210b5a9ce75d0ee099db269b2b7e4d8633669
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2020-12-17 16:32:54 -0600

    Updated Markdown Function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,8 +6,36 @@ function generateMarkdown(data) {
   ## Table of Contents
   [Description](#description)
   [Technologies Used](#technologies)
-  [Contributers](#contributors)
-  []
+  [Contributors](#contributors)
+  [Installation](#install)
+  [Usage](#usage)
+  [GitHub User](#github)
+  [GitHub Repo](#repo)
+  [License](#license)
+
+  ## Description
+  ${data.description}
+
+  ## Technologies Used
+  ${data.technologies}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Installation
+  Instructions: ${data.install}
+
+  ## Usage
+  Instructions : ${data.usage}
+
+  ## GitHub User
+  GitHub User Name: ${data.github}
+
+  ## GitHub Repo
+  GitHub Repo Name: ${data.repo}
+
+  ## License
+  license Used: ${data.license}
 
 `;
 }
